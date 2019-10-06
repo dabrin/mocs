8dcac57cb59c3f25d6d9107c0f2a7bd5a6cc544b
ALWAYS1996 <danivc@hotmail.es>
2019-10-06 14:36:34 -0600

    arreglos

---
+++ Sistema control de labores/Models/DiaSeleccionado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_control_de_labores.Models
+{
+    public class DiaSeleccionado
+    {
+        public int dia { get; set; }
+        public int mes { get; set; }
+        public int anno { get; set; }
+    }
+}
