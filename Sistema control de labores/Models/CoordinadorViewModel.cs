db1625e7899734f66a3df11c300d0a1caf8f7676
ALWAYS1996 <danivc@hotmail.es>
2019-10-08 15:16:17 -0600

    controladores

---
+++ Sistema control de labores/Models/CoordinadorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistema_control_de_labores.Models.ViewModel
+{
+    public class CoordinadorViewModel
+    {
+        public int TN_idUsuario { get; set;}
+        public int TN_idSubProceso { get; set;}
+        public DateTime TF_RealizaTarea { get; set;}
+        public int TN_Cantidad { get; set; }
+    }
+}
