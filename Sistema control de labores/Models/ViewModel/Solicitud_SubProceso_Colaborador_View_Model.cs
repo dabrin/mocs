e88f9e7649a51c6ce9aacc14c4e883343d9834c4
ALWAYS1996 <danivc@hotmail.es>
2019-10-08 14:33:25 -0600

    model y charts

---
+++ Sistema control de labores/Models/ViewModel/Solicitud_SubProceso_Colaborador_View_Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sistema_control_de_labores.Models.ViewModel
+{
+    public class Solicitud_SubProceso_Colaborador_View_Model
+    {
+        public int TN_idUsuarioSolicitante { get; set; }
+
+        public int TN_idUsuarioDestino { get; set; }
+        [Display(Name = "SubProceso")]
+        public int TN_idSubProceso { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha")]
+        public DateTime TF_SolicitudRealizada{ get; set; }
+        public int TC_Nom_SubProceso { get; set; }
+
+    }
+}
---
+++ Sistema control de labores/Models/ViewModel/SubProcesoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sistema_control_de_labores.Models.ViewModel
+{
+    public class SubProcesoViewModel
+    {
+        public int TN_idSubProceso { get; set; }
+        [Required]
+        [Display(Name = "SubProceso")]
+        public String TC_Nom_SubProceso { get; set; }
+    }
+}
