5cad2a21d1f4151f911dafb7cebd98d5ad35b113
LuisCa2492 <48541286+LuisCa2492@users.noreply.github.com>
2019-10-08 20:24:38 -0600

    coordinador Controller

--- Sistema control de labores/Controllers/CoordinadorController.cs
+++ Sistema control de labores/Controllers/CoordinadorController.cs
@@ -8,15 +8,103 @@ namespace Sistema_control_de_labores.Controllers
 {
     public class CoordinadorController : Controller
     {
-        // GET: Coordinador
+        public ActionResult CrearReporte()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearReporte(CrearReporteViewModel model)
+        {
+            try
+            {
+                //valida los dataAnnotations
+                if (ModelState.IsValid)
+                {
+                    using (sisContLabEntities db = new sisContLabEntities())
+                    {
+                        var reporte = new TSCL_Reporte_Usuario();
+                        reporte.TN_idUsuario = model.TN_idUsuario;
+                        reporte.TN_idSubProceso = model.TN_idSubProceso;
+                        reporte.TF_RealizaTarea = model.TF_RealizaTarea;
+                        reporte.TN_Cantidad = model.TN_Cantidad;
+                        db.PA_InsertarReporte(model.TN_idUsuario, model.TN_idSubProceso,
+                        model.TF_RealizaTarea, model.TN_Cantidad);
+                        db.SaveChangesAsync();
+                    }
+                    return Redirect("~/Coordinador/CrearReporte");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
         public ActionResult ControlUsuarios()
         {
             return View();
         }
 
-        public ActionResult Solicitudes()
+        public ActionResult Solicitud_SubProceso_Colaborador()
         {
+            obtenerSubProcesos();
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Solicitud_SubProceso_Colaborador(Solicitud_SubProceso_Colaborador_View_Model model)
+        {
+            try
+            {
+                DateTime date = DateTime.Now;
+                //valida los dataAnnotations
+                if (ModelState.IsValid)
+                {
+                    using (sisContLabEntities db = new sisContLabEntities())
+                    {
+                        var reporte = new TSCL_SolSubPCol();
+                        reporte.TN_idUsuarioSolicitante = model.TN_idUsuarioSolicitante;
+                        reporte.TN_idUsuarioDestino = model.TN_idUsuarioDestino;
+                        reporte.TN_idSubProceso = model.TN_idSubProceso;
+                        reporte.TF_SolicitudRealizada = model.TF_SolicitudRealizada;
+
+                        db.PA_Insertar_SolSubCol(model.TN_idUsuarioSolicitante, model.TN_idUsuarioDestino,
+                        model.TN_idSubProceso, date);
+                        db.SaveChangesAsync();
+                    }
+                    return Redirect("~/Coordinador/Solicitud_SubProceso_Colaborador");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+
+
+
+            //return View();
+        }
+
+        public List<SubProcesoViewModel> obtenerSubProcesos()
+        {
+            sisContLabEntities db = new sisContLabEntities();
+            List<SubProcesoViewModel> resultado = new List<SubProcesoViewModel>();
+            resultado = db.Database.SqlQuery<SubProcesoViewModel>("PA_Consulta_Todos_SubProceso").ToList();
+            ViewBag.subProcesos = resultado;
+
+            return resultado;
+        }
+
+
+    }
+}
+
+
+
     }
 }
